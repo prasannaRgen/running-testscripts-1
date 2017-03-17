2524098f7c40b5c609dc0a3e92339d1cb2a979dc
shrutika.mendhe <shrutika.mendhe@click2cloud.net>
2017-03-17 17:52:11 +0530

    Updated for chromedriver

--- src/com/uat/TestCases/VerifyLoginFucntionality.java
+++ src/com/uat/TestCases/VerifyLoginFucntionality.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
